8a9670326c316af8fc1c2ac9e5626ce7c9b039aa
Aritra <aritrabiswas9788@gmail.com>
2024-08-20 15:36:34 +0530

    Docs created

--- lib/coordinate.dart
+++ lib/coordinate.dart
@@ -1,9 +1,10 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart' as mp;
 
+///A class that describe a Coordinate on Earth
 class Coordinates {
   late double latitude;
   late double longitude;
-
+  ///Main Constructor for this class
   Coordinates({
     required this.latitude,
     required this.longitude,
@@ -18,6 +19,7 @@ class Coordinates {
     return LatLng(point.latitude, point.longitude);
   }*/
 
+  ///Copy Constructor from LatLng Object
   Coordinates.fromLatLngMap(mp.LatLng point) {
     latitude = point.latitude;
     longitude = point.longitude;
@@ -26,18 +28,19 @@ class Coordinates {
     longitude %= 360.0;
   }
 
+  ///Function to convert Coordinate to LatLng
   mp.LatLng toLatLngMap(Coordinates point)
   {
     return mp.LatLng(point.latitude, point.longitude);
   }
 
-  // Override toString for better debugging output
+  ///Override toString for better debugging output
   @override
   String toString() {
     return 'Coordinates(latitude: $latitude, longitude: $longitude)';
   }
 
-  // Optionally, you can still override == and hashCode for value comparison
+  /// Optionally, you can still override == and hashCode for value comparison
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
@@ -46,6 +49,7 @@ class Coordinates {
     return latitude == otherCoordinates.latitude && longitude == otherCoordinates.longitude;
   }
 
+  ///HashCode for Coordinate Function
   @override
   int get hashCode => latitude.hashCode ^ longitude.hashCode;
 }
